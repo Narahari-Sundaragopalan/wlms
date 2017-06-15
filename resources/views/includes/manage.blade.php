ac3512d76229ab1e5ad2881c6de8c384c4445905
Akshitha <asreedhar@unomaha.edu>
2017-06-15 13:39:06 -0600

    change in manager view ,left side bar and button

--- resources/views/includes/manage.blade.php
+++ resources/views/includes/manage.blade.php
@@ -1,7 +1,7 @@
 
 <div class="container" style="float: left;width: 20%; border-right: 2px solid black">
     <div style="height: 70%">
-        <img class="center-block" src="{{URL::asset('/images/man.jpg')}}" alt="profile Pic" height="150" width="200">
+        <img class="center-block" src="{{URL::asset('/images/man.jpg')}}" alt="profile Pic" height="150" width="150">
     </div>
     <div>
         <h4 style="text-align: center; color: black"> Manager </h4>
@@ -10,7 +10,7 @@
 
     <div>
         <li style="list-style: none;">
-            <ul ><a href="{{ url('/viewscheduler') }}" class="btn btn-danger" style="width: 150px; height: 30px;" >View Schedule</a></ul>
+            <ul ><a href="" class="btn btn-danger" style="width: 150px; height: 30px;" >View Schedule</a></ul>
 
         </li>
     </div>
